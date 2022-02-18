3c9265d593a4b2bfc1872408a17f84266843bd11
Sk-Monir <96935864+Sk-Monir@users.noreply.github.com>
2022-02-18 11:37:14 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+var a = 12;
+var b =5;
+document.write("Addition : "+ (a+b) +"<br>");
+document.write("Subtraction : " + (a-b) + "<br>");
+document.write("Multiplication : "+ (a*b ) +"<br>");
+document.write("Division : " + (a/b) +"<br>");
+document.write("Modulus : " + (a%b) +"<br>");
+document.write("Exponention : " + (a**b)+"<br>");
